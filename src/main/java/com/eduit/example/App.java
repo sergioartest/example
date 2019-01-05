01a460fba5ea1c940afb77fd23d0c28bc358f155
Sergio Carrasco <44214303+sergioardev@users.noreply.github.com>
2019-01-05 03:28:01 -0300

    Initial commit

---
+++ src/main/java/com/eduit/example/App.java
@@ -0,0 +1,14 @@
+package com.eduit.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+    public static void main( String[] args ){
+    	final String name = StringUtils.capitalize("sergio");
+    	System.out.println(name);
+    }
+}
